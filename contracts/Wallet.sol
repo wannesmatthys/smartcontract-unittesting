63310c12792f592e357ce29574be401842570179
Wannes Matthys <wannesmatthys@gmail.com>
2022-02-07 16:25:04 +0100

    Create a smartcontract to send and receive money

---
+++ contracts/Wallet.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Wallet {
+
+    mapping(address => uint256) _balances;
+
+    function sendMoney(address _to, uint256 _amount) external {
+        require(_balances[msg.sender] >= _amount, "ERROR: You do not have this much ether!");
+
+        _balances[msg.sender] -= _amount;
+        _balances[_to] += _amount; 
+    }
+
+    receive() payable external {
+        _balances[msg.sender] += msg.value;
+    }
+
+    function getBalance(address _from) external view returns(uint256) {
+        require(_from != address(0), "ERROR: Invalid address");
+        return _balances[_from];
+    }
+
+}
